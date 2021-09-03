7d95c4fd7928b3d186bb0631e84ae10976adbd05
AHpx <ahpx@yandex.com>
2021-09-03 22:32:12 +0800

    Setup bot entry

---
+++ LithiumBot/Modules/BasicModule.cs
@@ -0,0 +1,23 @@
+﻿using Mirai.Net.Data.Messages;
+using Mirai.Net.Data.Messages.Concretes;
+using Mirai.Net.Data.Messages.Receivers;
+using Mirai.Net.Data.Modules;
+using Mirai.Net.Modules;
+using Mirai.Net.Utils.Scaffolds;
+
+namespace LithiumBot.Modules
+{
+    public class BasicModule : ICommandModule
+    {
+        [CommandTrigger("help")]
+        public async void Execute(MessageReceiverBase r, MessageBase executeMessage)
+        {
+            if (r is GroupMessageReceiver receiver)
+            {
+                await receiver.SendGroupMessageAsync("大抵是锂机器人罢。\r\n".Append("https://github.com/SinoAHpx/LithiumBot"));
+            }
+        }
+
+        public bool? IsEnable { get; set; }
+    }
+}
--- LithiumBot/Program.cs
+++ LithiumBot/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LithiumBot.Modules;
+using Mirai.Net.Data.Messages.Receivers;
+using Mirai.Net.Modules;
+using Mirai.Net.Sessions;
+using Mirai.Net.Utils.Scaffolds;
+using Spectre.Console;
 
 namespace LithiumBot
 {
-    class Program
+    public static class Program
     {
-        private static void Main(string[] args)
+        public static List<ICommandModule> Modules { get; set; }
+
+        public static string ModulesStr { get; set; }
+
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MiraiBot bot = null;
+            await AnsiConsole.Status()
+                .Spinner(Spinner.Known.Star)
+                .StartAsync("Launching...", async ctx =>
+                {
+                    #region Mirai bot
+
+                    AnsiConsole.MarkupLine("Creating primary MiraiBot instance...");
+                    bot = new MiraiBot
+                    {
+                        Address = "localhost:8080",
+                        QQ = "2672886221",
+                        VerifyKey = "1145141919810"
+                    };
+
+                    await bot.LaunchAsync();
+                    AnsiConsole.MarkupLine("MiraiBot created...");
+
+
+                    #endregion
+
+                    #region Modules
+
+                    ctx.Status("Loading modules....");
+
+                    Modules = CommandScaffold.LoadCommandModules<BasicModule>().ToList();
+                    ModulesStr = string.Join("\n",
+                        Modules.Where(x => x.IsEnable is not false).Select(x => x.GetType().Name));
+                    AnsiConsole.MarkupLine(
+                        $"Following modules loaded: {ModulesStr}");
+
+                    #endregion
+
+                    #region Subscribers
+
+                    ctx.Status("Subscribing websocket notifications....");
+
+                    AnsiConsole.MarkupLine("Listening to GroupMessage...");
+
+                    bot.MessageReceived
+                        .WhereAndCast<GroupMessageReceiver>()
+                        .Subscribe(x =>
+                        {
+                            x.ExecuteCommandModules(Modules);
+                        });
+
+                    #endregion
+                });
+
+            while (!AnsiConsole.Confirm("Exit?"))
+            {
+            }
+
+            bot.Dispose();
         }
     }
 }
