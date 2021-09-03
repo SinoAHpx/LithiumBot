c4ec83f66e0f5cbc4d7536ae62a97ec9bd1436e5
AHpx <ahpx@yandex.com>
2021-09-03 21:12:10 +0800

    Add project files.

---
+++ LithiumBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LithiumBot
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
